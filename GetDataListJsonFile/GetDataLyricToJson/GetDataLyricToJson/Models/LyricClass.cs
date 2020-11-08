1d82b30f5cd5ec3d54d4fb6c752eb0c35ccce916
hhpr98 <39483775+hhpr98@users.noreply.github.com>
2020-11-09 03:05:30 +0700

    feat: get infor file to list

---
+++ GetDataListJsonFile/GetDataLyricToJson/GetDataLyricToJson/Models/LyricClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GetDataLyricToJson.Models
+{
+    class LyricClass
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string composer { get; set; }
+        public string filename { get; set; }
+        public string url { get; set; }
+        public string type { get; set; }
+    }
+}
---
+++ GetDataListJsonFile/GetDataLyricToJson/GetDataLyricToJson/Program.cs
@@ -0,0 +1,69 @@
+﻿using GetDataLyricToJson.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace GetDataLyricToJson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"C:\git\LyricApp\LoiBaiHat2";
+            var filePaths = Directory.GetFiles(path);
+            // "C:\git\LyricApp\LoiBaiHat\1 Lần - Tr?ng Hiếu .docx"
+            List<LyricClass> list = new List<LyricClass>();
+            int i = 1;
+            foreach (var item in filePaths)
+            {
+                LyricClass tmp = new LyricClass();
+
+                var _backslackIdx = item.LastIndexOf(@"\");
+                var _minusIdx = item.LastIndexOf("-");
+                var _dotIdx = item.LastIndexOf(".");
+
+                // id
+                tmp.id = i;
+
+                // name
+                var _name = item.Substring(_backslackIdx + 1, _minusIdx - _backslackIdx - 1);
+                while (_name[0] == ' ')
+                {
+                    _name = _name.Substring(1, _name.Length - 1);
+                }
+                while (_name[_name.Length - 1] == ' ')
+                {
+                    _name = _name.Substring(0, _name.Length - 1);
+                }
+                tmp.name = _name;
+
+                // composer
+                var _composer = item.Substring(_minusIdx + 1, _dotIdx - _minusIdx - 1);
+                while (_composer[0] == ' ')
+                {
+                    _composer = _composer.Substring(1, _composer.Length - 1);
+                }
+                while (_composer[_composer.Length - 1] == ' ')
+                {
+                    _composer = _composer.Substring(0, _composer.Length - 1);
+                }
+                tmp.composer = _composer;
+
+                // filename
+                var _fileName = item.Substring(_backslackIdx + 1, item.Length - _backslackIdx - 1);
+                tmp.filename = _fileName;
+
+                // url
+                tmp.url = item;
+
+                // type
+                var _type = item.Substring(_dotIdx + 1, item.Length - _dotIdx - 1);
+                tmp.type = _type;
+
+                list.Add(tmp);
+
+                i++;
+            }
+        }
+    }
+}
