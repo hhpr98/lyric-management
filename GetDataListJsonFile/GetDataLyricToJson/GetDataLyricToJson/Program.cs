e57887265ed57fcad10134bff2f75c98a5c2f888
hhpr98 <39483775+hhpr98@users.noreply.github.com>
2020-11-09 22:53:48 +0700

    fix: "-" with "– " character

--- GetDataListJsonFile/GetDataLyricToJson/GetDataLyricToJson/Program.cs
+++ GetDataListJsonFile/GetDataLyricToJson/GetDataLyricToJson/Program.cs
@@ -17,52 +17,16 @@ namespace GetDataLyricToJson
             int i = 1;
             foreach (var item in filePaths)
             {
-                LyricClass tmp = new LyricClass();
-
-                var _backslackIdx = item.LastIndexOf(@"\");
-                var _minusIdx = item.LastIndexOf("-");
-                var _dotIdx = item.LastIndexOf(".");
-
-                // id
-                tmp.id = i;
-
-                // name
-                var _name = item.Substring(_backslackIdx + 1, _minusIdx - _backslackIdx - 1);
-                while (_name[0] == ' ')
+                try
                 {
-                    _name = _name.Substring(1, _name.Length - 1);
+                    var lyr = getLyricInfo(item, i);
+                    list.Add(lyr);
                 }
-                while (_name[_name.Length - 1] == ' ')
+                catch
                 {
-                    _name = _name.Substring(0, _name.Length - 1);
+                    Console.WriteLine("Lỗi at: " + item);
                 }
-                tmp.name = _name;
-
-                // composer
-                var _composer = item.Substring(_minusIdx + 1, _dotIdx - _minusIdx - 1);
-                while (_composer[0] == ' ')
-                {
-                    _composer = _composer.Substring(1, _composer.Length - 1);
-                }
-                while (_composer[_composer.Length - 1] == ' ')
-                {
-                    _composer = _composer.Substring(0, _composer.Length - 1);
-                }
-                tmp.composer = _composer;
-
-                // filename
-                var _fileName = item.Substring(_backslackIdx + 1, item.Length - _backslackIdx - 1);
-                tmp.filename = _fileName;
-
-                // url
-                tmp.url = item;
-
-                // type
-                var _type = item.Substring(_dotIdx + 1, item.Length - _dotIdx - 1);
-                tmp.type = _type;
-
-                list.Add(tmp);
-
+               
                 i++;
             }
 
@@ -76,6 +40,55 @@ namespace GetDataLyricToJson
         {
             DateTime d = DateTime.Now;
             return d.Year + "-" + d.Month + "-" + d.Day + " " + d.Hour + "h" + d.Minute + "m" + d.Second + "s.json";
+        }
+
+        public static LyricClass getLyricInfo(string item, int i)
+        {
+            LyricClass tmp = new LyricClass();
+
+            var _backslackIdx = item.LastIndexOf(@"\");
+            var _minusIdx = item.LastIndexOf("-");
+            var _dotIdx = item.LastIndexOf(".");
+
+            // id
+            tmp.id = i;
+
+            // name
+            var _name = item.Substring(_backslackIdx + 1, _minusIdx - _backslackIdx - 1);
+            while (_name[0] == ' ')
+            {
+                _name = _name.Substring(1, _name.Length - 1);
+            }
+            while (_name[_name.Length - 1] == ' ')
+            {
+                _name = _name.Substring(0, _name.Length - 1);
+            }
+            tmp.name = _name;
+
+            // composer
+            var _composer = item.Substring(_minusIdx + 1, _dotIdx - _minusIdx - 1);
+            while (_composer[0] == ' ')
+            {
+                _composer = _composer.Substring(1, _composer.Length - 1);
+            }
+            while (_composer[_composer.Length - 1] == ' ')
+            {
+                _composer = _composer.Substring(0, _composer.Length - 1);
+            }
+            tmp.composer = _composer;
+
+            // filename
+            var _fileName = item.Substring(_backslackIdx + 1, item.Length - _backslackIdx - 1);
+            tmp.filename = _fileName;
+
+            // url
+            tmp.url = item;
+
+            // type
+            var _type = item.Substring(_dotIdx + 1, item.Length - _dotIdx - 1);
+            tmp.type = _type;
+
+            return tmp;
         }
     }
 }
