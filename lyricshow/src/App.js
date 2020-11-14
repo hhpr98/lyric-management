fe7082f01512375bdec7ff9a911578801e1b47fa
hhpr98 <39483775+hhpr98@users.noreply.github.com>
2020-11-15 06:21:12 +0700

    feat: lyric app OK

--- lyricshow/src/App.js
+++ lyricshow/src/App.js
@@ -1,24 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import 'antd/dist/antd.css';
+import { Input, Row, Col, Table, Tag, Space } from 'antd';
+import swal from 'sweetalert';
+import lyricdata from "./data/2020-11-9 23h7m41s.json";
+import { useEffect, useState } from 'react';
+
+const { Search } = Input;
 
 function App() {
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    setData(lyricdata);
+  }, []);
+
+  const onSearch = value => {
+
+    if (value === "") {
+      setData(lyricdata);
+      return;
+    }
+
+    const dataSearch = lyricdata.filter(item => item.name.toLocaleLowerCase().includes(value.toLocaleLowerCase()) || item.composer.toLocaleLowerCase().includes(value.toLocaleLowerCase()));
+
+    if (dataSearch.length === 0) {
+      setData([]);
+      swal({
+        title: "Thông báo",
+        text: "Không tìm thấy bài hát!",
+        icon: "error",
+        button: "Đồng ý",
+      });
+    } else {
+      setData(dataSearch);
+      console.log(data);
+      swal({
+        title: "Thông báo",
+        text: "Thành công!",
+        icon: "success",
+        button: "Đồng ý",
+      });
+    }
+
+  }
+
+  const columns = [
+    {
+      title: 'id',
+      dataIndex: 'id',
+      key: 'id',
+    },
+    {
+      title: 'Tên bài hát',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Nhạc sĩ',
+      dataIndex: 'composer',
+      key: 'composer',
+    }
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Row>
+      <Col span={16} offset={4} style={{ marginTop: "50px" }}>
+        <Search
+          placeholder="Nhập tên bài hát..."
+          allowClear
+          enterButton="Tìm kiếm"
+          size="large"
+          onSearch={onSearch}
+        />
+      </Col>
+      <Col span={16} offset={4} style={{ marginTop: "50px" }}>
+        <Table columns={columns} dataSource={data} />
+      </Col>
+    </Row>
   );
 }
 
