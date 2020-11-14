a9b9284ccfa414e98fcebd2207e593a331d24dc3
hhpr98 <39483775+hhpr98@users.noreply.github.com>
2020-11-15 06:47:25 +0700

    every is OK

--- lyricshow/src/App.js
+++ lyricshow/src/App.js
@@ -1,5 +1,7 @@
 import 'antd/dist/antd.css';
-import { Input, Row, Col, Table, Tag, Space } from 'antd';
+import { Input, Row, Col, Table, Space, Button, } from 'antd';
+import Highlighter from 'react-highlight-words';
+import { SearchOutlined } from '@ant-design/icons';
 import swal from 'sweetalert';
 import lyricdata from "./data/2020-11-9 23h7m41s.json";
 import { useEffect, useState } from 'react';
@@ -9,6 +11,8 @@ const { Search } = Input;
 function App() {
 
   const [data, setData] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  const [searchedColumn, setSearchedColumn] = useState("");
 
   useEffect(() => {
     setData(lyricdata);
@@ -44,21 +48,89 @@ function App() {
 
   }
 
+  const getColumnSearchProps = dataIndex => ({
+    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
+      <div style={{ padding: 8 }}>
+        <Input
+          // ref={node => {
+          //   searchInput = node;
+          // }}
+          placeholder={"Tìm kiếm ..."}
+          value={selectedKeys[0]}
+          onChange={e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+          onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+          style={{ width: 188, marginBottom: 8, display: 'block' }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Tìm
+          </Button>
+          <Button onClick={() => handleReset(clearFilters)} size="small" style={{ width: 90 }}>
+            Reset
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: filtered => <SearchOutlined style={{ color: filtered ? '#1890ff' : undefined }} />,
+    onFilter: (value, record) =>
+      record[dataIndex]
+        ? record[dataIndex].toString().toLowerCase().includes(value.toLowerCase())
+        : '',
+    onFilterDropdownVisibleChange: visible => {
+      if (visible) {
+        // setTimeout(() => searchInput.select(), 100);
+      }
+    },
+    render: text =>
+      searchedColumn === dataIndex ? (
+        <Highlighter
+          highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
+          searchWords={[searchText]}
+          autoEscape
+          textToHighlight={text ? text.toString() : ''}
+        />
+      ) : (
+          text
+        ),
+  });
+
+  const handleSearch = (selectedKeys, confirm, dataIndex) => {
+    confirm();
+    setSearchText(selectedKeys[0]);
+    setSearchedColumn(dataIndex);
+  };
+
+  const handleReset = clearFilters => {
+    clearFilters();
+    setSearchText("");
+  };
+
   const columns = [
     {
       title: 'id',
       dataIndex: 'id',
       key: 'id',
+      width: '10%',
     },
     {
       title: 'Tên bài hát',
       dataIndex: 'name',
       key: 'name',
+      width: '50%',
+      ...getColumnSearchProps('name'),
     },
     {
       title: 'Nhạc sĩ',
       dataIndex: 'composer',
       key: 'composer',
+      width: '40%',
+      ...getColumnSearchProps('composer'),
     }
   ];
 
